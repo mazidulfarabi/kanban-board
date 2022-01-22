f01c8a19e718ab34b9e6a9736081e687ad00d38d
Md. Mazidul Haque Farabi <mhfarabi09@gmail.com>
2022-01-23 02:21:44 +0600

    css

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
 import Kanban from "./view/Kanban.js";
 
+var header = $('body');
+
+var backgrounds = new Array(
+    'url(http://placekitten.com/100)'
+  , 'url(http://placekitten.com/200)'
+  , 'url(http://placekitten.com/300)'
+  , 'url(http://placekitten.com/400)'
+);
+    
+var current = 0;
+
+function nextBackground() {
+    current++;
+    current = current % backgrounds.length;
+    header.css('background-image', backgrounds[current]);
+}
+setInterval(nextBackground, 1000);
+
+header.css('background-image', backgrounds[0]);
+
 new Kanban(
 	document.querySelector(".kanban")
 );
