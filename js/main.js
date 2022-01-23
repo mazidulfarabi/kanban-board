51236f1c3e6b7201d16c54d8abfdbd79616fc939
Md. Mazidul Haque Farabi <mhfarabi09@gmail.com>
2022-01-23 14:12:02 +0600

    css

--- js/main.js
+++ js/main.js
@@ -2,30 +2,13 @@ import Kanban from "./view/Kanban.js";
 
 var header = $('body');
 
-var img1, img2, img3, img4, img5, img6;
-
-if (document.images) {
-  img1 = new Image();
-  img1.src = "./img/1.webp";
-  img2 = new Image();
-  img2.src = "./img/2.webp";
-  img3 = new Image();
-  img3.src = "./img/3.webp";
-  img4 = new Image();
-  img4.src = "./img/4.webp";
-  img5 = new Image();
-  img5.src = "./img/5.webp";
-  img6 = new Image();
-  img6.src = "./img/6.webp";
-}
-
 var backgrounds = new Array(
-    img1,
-    img2,
-    img3,
-    img4,
-    img5,
-    img6
+    'url(./img/1.webp)'
+  , 'url(./img/2.webp)'
+  , 'url(./img/3.webp)'
+  , 'url(./img/4.webp)'
+  , 'url(./img/5.webp)'
+  , 'url(./img/6.webp)'
 );
     
 var current = 0;
