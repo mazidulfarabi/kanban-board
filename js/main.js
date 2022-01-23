91231ef1c12b64fca66d1ca3138d097dfce22215
Md. Mazidul Haque Farabi <mhfarabi09@gmail.com>
2022-01-24 01:56:35 +0600

    Revert "revert"
    This reverts commit f611a153e3bc7c3df223afce0db3a7f50f201929.

--- js/main.js
+++ js/main.js
@@ -1,29 +1,5 @@
 import Kanban from "./view/Kanban.js";
 
-var i = 0;
-var images = [];
-var slideTime = 5000; // 3 seconds
-
-images[0] = '../img/2.webp';
-images[1] = '../img/3.webp';
-images[2] = '../img/4.webp';
-images[3] = '../img/5.webp';
-images[4] = '../img/6.webp';
-images[5] = '../img/1.webp';
-
-function changePicture() {
-    document.body.style.backgroundImage = "url(" + images[i] + ")";
-
-    if (i < images.length - 1) {
-        i++;
-    } else {
-        i = 0;
-    }
-    setTimeout(changePicture, slideTime);
-}
-
-window.onload = setTimeout(changePicture, slideTime);
-
 new Kanban(
 	document.querySelector(".kanban")
 );
