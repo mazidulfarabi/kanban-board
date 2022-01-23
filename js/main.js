0c001d9cf2c3e58acf8ff522166229a8bdb10cbd
Md. Mazidul Haque Farabi <mhfarabi09@gmail.com>
2022-01-23 13:38:33 +0600

    css

--- js/main.js
+++ js/main.js
@@ -10,7 +10,24 @@ var backgrounds = new Array(
   , 'url(./img/5.webp)'
   , 'url(./img/6.webp)'
 );
-    
+
+var images = [];
+function preload() {
+    for (var i = 0; i < arguments.length; i++) {
+        images[i] = new Image();
+        images[i].src = preload.arguments[i];
+    }
+}
+
+preload(
+  "./img/1.webp)",
+  "./img/1.webp)",
+  "./img/3.webp)",
+  "./img/4.webp)",
+  "./img/5.webp)",
+  "./img/6.webp)",
+)
+
 var current = 0;
 
 function nextBackground() {
